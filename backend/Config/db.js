610d7f8120c907ecfe212e3504ed272c4e9e5162
Hemant142 <yhemant203@gmail.com>
2025-05-27 22:27:24 +0530

    Updared

--- backend/Config/db.js
+++ backend/Config/db.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 let connection =async () => {
     try {
-         await mongoose.connect(process.env.MONGO_URI);
+         await mongoose.connect(process.env.MONGODBURI);
         console.log(`MongoDB connected: `);
     } catch (error) {
         console.error(`Error: ${error.message}`);
