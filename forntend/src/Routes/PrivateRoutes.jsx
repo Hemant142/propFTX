4bb9413e35017039165b62f6070d59fff5a8d371
Hemant142 <yhemant203@gmail.com>
2025-05-28 10:23:53 +0530

    Updated fronend

--- forntend/src/Routes/PrivateRoutes.jsx
+++ forntend/src/Routes/PrivateRoutes.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
-import { Navigate, Outlet } from 'react-router-dom';
+import { Navigate, Outlet, useNavigate } from 'react-router-dom';
+import Login from '../Pages/Login';
 // import { useAuth } from '../context/AuthContext';
 
 // Usage: <PrivateRoutes role="admin" />
 export default function PrivateRoutes({ role }) {
   const user = JSON.parse(localStorage.getItem('user'));
   const storedToken = localStorage.getItem('token');
+  const navigate = useNavigate();
 console.log("PrivateRoutes user:", user);
+
   if (!user || !storedToken) {
-    return <Navigate to="/login" />;
+    return <Login />;
   }
 
   if (role && user.role !== role) {
     // If role is specified and doesn't match, redirect
-    return <Navigate to="/" />;
+    return navigate("/") ;
   }
 
   return <Outlet />;
